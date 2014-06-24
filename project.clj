82bf6e316231381826c75d8682bec685a4934f83
greenyouse <greenyouse@hotmail.com>
2014-06-23 21:31:46 -0500

    updated project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,3 @@
 (defproject ydn-cljs "1.0.1"
   :description "YDN tools for ClojureScript"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"})
+  :url "http://dev.yathit.com/")
