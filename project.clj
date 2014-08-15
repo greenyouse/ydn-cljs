da232c5b7eef3db3d9b2be3ee3bf4618c66d1ebc
greenyouse <greenyouse@hotmail.com>
2014-08-15 01:35:35 -0500

    Added the license from ydn project

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject com.ydn/db "1.0.3"
   :description "YDN database tools packaged for ClojureScript"
-  :url "http://dev.yathit.com/")
+  :url "http://dev.yathit.com/"
+  :license "Apache License, Version 2.0")
